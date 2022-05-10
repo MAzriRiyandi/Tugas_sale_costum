db605673612c76a176d2df0b8c708107df4b2e42
MAzriRiyandi <Azri.Riyandi@hashmicro.com>
2022-05-10 16:47:58 +0700

    update_sale

--- azri_sale/models/sale.py
+++ azri_sale/models/sale.py
@@ -4,18 +4,19 @@ from odoo.exceptions import UserError
 
 
 class SaleOrder(models.Model):
-    _name = 'sale.order'
     _inherit = 'sale.order'
 
     def create_mo(self):
         for record in self:
-            bom = record.order_line.product_id.bom_ids.bom_id
-            if bom:
-                self.env['mrp.production'].create({
-                    'product_id': record.order_line.product_id.id,
-                    'product_qty': record.order_line.product_uom_qty,
-                    'product_uom_id': record.order_line.product_uom_id,
-                    'bom_id': record.order_line.product_id.bom_ids.id,
-                })
-            else:
-                pass
+            for i in record.order_line:
+                bom = i.product_id.bom_ids
+                if bom:
+                    for l in bom:
+                        self.env['mrp.production'].create({
+                            'product_id': i.product_id.id,
+                            'product_qty': i.product_uom_qty,
+                            'product_uom_id': i.product_uom.id,
+                            'bom_id': l.id,
+                            })
+                else:
+                    pass
